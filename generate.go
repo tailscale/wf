a4684a7915bf8404ff9c0fac4a9b9bb4a2ac3c88
David Anderson <danderson@tailscale.com>
2021-02-28 13:22:59 -0800

    Refactor everything into firewall.go, add stringers.
    Signed-off-by: David Anderson <danderson@tailscale.com>

--- generate.go
+++ generate.go
@@ -2,3 +2,14 @@ package wf
 
 //go:generate go run generators/gen_guids.go includes/fwpmu.h zguids.go
 //go:generate go run golang.org/x/sys/windows/mkwinsyscall -output zsyscall_windows.go syscall.go
+
+//go:generate stringer -output=zlayerflags_strings.go -type=LayerFlags -trimprefix=LayerFlags
+//go:generate stringer -output=zfieldtype_strings.go -type=FieldType -trimprefix=Fieldtype
+//go:generate stringer -output=zsublayerflags_strings.go -type=SublayerFlags -trimprefix=SublayerFlags
+//go:generate stringer -output=zfilterenumtype_strings.go -type=FilterEnumType -trimprefix=FilterEnumType
+//go:generate stringer -output=zfilterenumflags_strings.go -type=FilterEnumFlags -trimprefix=FilterEnumFlags
+//go:generate stringer -output=zactiontype_strings.go -type=ActionType -trimprefix=ActionType
+//go:generate stringer -output=zmatchtype_strings.go -type=MatchType -trimprefix=MatchType
+//go:generate stringer -output=zfilterflags_strings.go -type=FilterFlags -trimprefix=FilterFlags
+//go:generate stringer -output=zproviderflags_strings.go -type=ProviderFlags -trimprefix=ProviderFlags
+//go:generate stringer -output=zdatatype_strings.go -type=DataType -trimprefix=DataType
