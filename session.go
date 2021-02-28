1e36233899d4f16eb6221b6ce755910ce5cdb4f1
David Anderson <danderson@tailscale.com>
2021-02-27 16:24:58 -0800

    Add an accessor for layers.
    Layers are fairly isolated, you can only read them out. But if you're
    trying to dump the entire firewall, for example, being able to list
    out all layers is pretty great.
    
    Signed-off-by: David Anderson <danderson@tailscale.com>

--- session.go
+++ session.go
@@ -1,10 +1,6 @@
 package winfirewall
 
 import (
-	"fmt"
-	"reflect"
-	"unsafe"
-
 	"golang.org/x/sys/windows"
 )
 
@@ -39,31 +35,4 @@ func (s *Session) Close() error {
 		return nil
 	}
 	return fwpmEngineClose0(s.handle)
-}
-
-func (s *Session) Test() error {
-	var enumHandle windows.Handle
-	if err := fwpmLayerCreateEnumHandle0(s.handle, nil, &enumHandle); err != nil {
-		return err
-	}
-	defer fwpmLayerDestroyEnumHandle0(s.handle, enumHandle)
-
-	var layersArray **fwpmLayer0
-	var numLayers uint32
-	if err := fwpmLayerEnum0(s.handle, enumHandle, 1000, &layersArray, &numLayers); err != nil {
-		return err
-	}
-	defer fwpmFreeMemory0((**uintptr)(unsafe.Pointer(layersArray)))
-
-	var layers []*fwpmLayer0
-	sh := (*reflect.SliceHeader)(unsafe.Pointer(&layers))
-	sh.Cap = int(numLayers)
-	sh.Len = int(numLayers)
-	sh.Data = uintptr(unsafe.Pointer(layersArray))
-
-	for i, layer := range layers {
-		fmt.Printf("%d\n%#v\n%s\n%s\n%s\n%s\n\n", i, *layer, guidNames[layer.LayerKey], guidNames[layer.DefaultSubLayerKey], windows.UTF16PtrToString(layer.DisplayData.Name), windows.UTF16PtrToString(layer.DisplayData.Name))
-	}
-
-	return nil
 }
