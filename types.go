89ac30366ec87520619e0eeb3e5bff0a1763657f
David Anderson <danderson@tailscale.com>
2021-02-28 00:53:28 -0800

    Add value parser/packet, and start reading out filters.
    Filters are very complex to parse and serialize, so this doesn't
    yet dump the meat of the filters, just the general scaffolding.
    
    Signed-off-by: David Anderson <danderson@tailscale.com>

--- types.go
+++ types.go
@@ -74,8 +74,8 @@ type DataType uint32
 const (
 	DataTypeEmpty DataType = iota
 	DataTypeUint8
-	DataTypeUint6
-	DataTypeUint2
+	DataTypeUint16
+	DataTypeUint32
 	DataTypeUint64
 	DataTypeInt8
 	DataTypeInt16
@@ -127,4 +127,121 @@ type fwpmProvider0 struct {
 	Flags        ProviderFlags
 	ProviderData fwpByteBlob
 	ServiceName  *uint16
+}
+
+type FilterFlags uint32
+
+const (
+	FilterFlagsPersistent FilterFlags = 1 << iota
+	FilterFlagsBootTime
+	FilterFlagsHasProviderContext
+	FilterFlagsClearActionRight
+	FilterFlagsPermitIfCalloutUnregistered
+	FilterFlagsDisabled
+	FilterFlagsIndexed
+)
+
+type fwpValue0 struct {
+	Type  DataType
+	Value uintptr // unioned value
+}
+
+type fwpmFilter0 struct {
+	FilterKey           windows.GUID
+	DisplayData         fwpmDisplayData0
+	Flags               FilterFlags
+	ProviderKey         *windows.GUID
+	ProviderData        fwpByteBlob
+	LayerKey            windows.GUID
+	SubLayerKey         windows.GUID
+	Weight              fwpValue0
+	NumFilterConditions uint32
+	FilterConditions    *fwpmFilterCondition0
+	Action              Action
+	ProviderContextKey  windows.GUID
+	Reserved            *windows.GUID
+	FilterID            uint64
+	EffectiveWeight     fwpValue0
+}
+
+type ActionType uint32
+
+const (
+	ActionTypeBlock              ActionType = 0x1001
+	ActionTypePermit                        = 0x1002
+	ActionTypeCalloutTerminating            = 0x5003
+	ActionTypeCalloutInspection             = 0x6004
+	ActionTypeCalloutUnknown                = 0x4005
+)
+
+type MatchType uint32
+
+const (
+	MatchEqual MatchType = iota
+	MatchGreater
+	MatchLess
+	MatchGreaterOrEqual
+	MatchLessOrEqual
+	MatchRange
+	MatchFlagsAllSet
+	MatchFlagsAnySet
+	MatchFlagsNoneSet
+	MatchEqualCaseInsensitive
+	MatchNotEqual
+	MatchPrefix
+	MatchNotPrefix
+)
+
+type fwpConditionValue0 struct {
+	Type  DataType
+	Value uintptr
+}
+
+type fwpmFilterCondition0 struct {
+	FieldKey       windows.GUID
+	MatchType      MatchType
+	ConditionValue fwpConditionValue0
+}
+
+type fwpV4AddrAndMask struct {
+	Addr, Mask uint32
+}
+
+type fwpV6AddrAndMask struct {
+	Addr         [16]byte
+	PrefixLength uint8
+}
+
+type FilterEnumType uint32
+
+const (
+	FilterEnumTypeFullyContained FilterEnumType = iota
+	FilterEnumTypeOverlapping
+)
+
+type FilterEnumFlags uint32
+
+const (
+	FilterEnumFlagsBestTerminatingMatch FilterEnumFlags = iota + 1
+	FilterEnumFlagsSorted
+	FilterEnumFlagsBootTimeOnly
+	FilterEnumFlagsIncludeBootTime
+	FilterEnumFlagsIncludeDisabled
+)
+
+type fwpmProviderContextEnumTemplate0 struct {
+	ProviderKey         *windows.GUID
+	ProviderContextType uint32
+}
+
+type fwpmFilterEnumTemplate0 struct {
+	ProviderKey             *windows.GUID
+	LayerKey                windows.GUID
+	EnumType                FilterEnumType
+	Flags                   FilterEnumFlags
+	ProviderContextTemplate *fwpmProviderContextEnumTemplate0 // TODO: wtf?
+	NumConditions           uint32
+	Conditions              *fwpmFilterCondition0
+	ActionMask              uint32
+	CalloutKey              *windows.GUID
 }
