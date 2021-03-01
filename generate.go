8d8fe20382e119400200ee769ebdb047bc091370
David Anderson <danderson@tailscale.com>
2021-02-28 19:30:52 -0800

    Unexport Filter-related things for now.
    The visible API for filters needs more work and needs to
    be more divorced from the internal structures.
    
    Signed-off-by: David Anderson <danderson@tailscale.com>

--- generate.go
+++ generate.go
@@ -6,10 +6,10 @@ package wf
 //go:generate stringer -output=zlayerflags_strings.go -type=fwpmLayerFlags -trimprefix=fwpmLayerFlags
 //go:generate stringer -output=zfieldtype_strings.go -type=fwpmFieldType -trimprefix=fwpmFieldtype
 //go:generate stringer -output=zsublayerflags_strings.go -type=fwpmSublayerFlags -trimprefix=fwpmSublayerFlags
-//go:generate stringer -output=zfilterenumtype_strings.go -type=FilterEnumType -trimprefix=FilterEnumType
-//go:generate stringer -output=zfilterenumflags_strings.go -type=FilterEnumFlags -trimprefix=FilterEnumFlags
-//go:generate stringer -output=zactiontype_strings.go -type=ActionType -trimprefix=ActionType
-//go:generate stringer -output=zmatchtype_strings.go -type=MatchType -trimprefix=MatchType
-//go:generate stringer -output=zfilterflags_strings.go -type=FilterFlags -trimprefix=FilterFlags
+//go:generate stringer -output=zfilterenumtype_strings.go -type=filterEnumType -trimprefix=filterEnumType
+//go:generate stringer -output=zfilterenumflags_strings.go -type=filterEnumFlags -trimprefix=filterEnumFlags
+//go:generate stringer -output=zactiontype_strings.go -type=actionType -trimprefix=actionType
+//go:generate stringer -output=zmatchtype_strings.go -type=matchType -trimprefix=matchType
+//go:generate stringer -output=zfilterflags_strings.go -type=filterFlags -trimprefix=filterFlags
 //go:generate stringer -output=zproviderflags_strings.go -type=fwpmProviderFlags -trimprefix=fwpmProviderFlags
-//go:generate stringer -output=zdatatype_strings.go -type=DataType -trimprefix=DataType
+//go:generate stringer -output=zdatatype_strings.go -type=dataType -trimprefix=dataType
