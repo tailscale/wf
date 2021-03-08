4a7eb9bbb1255bc4afdc352ff681136b8e446893
David Anderson <danderson@tailscale.com>
2021-03-07 21:50:19 -0800

    Reorganize syscall.go a little.
    Signed-off-by: David Anderson <danderson@tailscale.com>

--- syscall.go
+++ syscall.go
@@ -1,47 +1,32 @@
 package wf
 
 //sys fwpmEngineOpen0(mustBeNil *uint16, authnService authnService, authIdentity *uintptr, session *fwpmSession0, engineHandle *windows.Handle) (err error) [failretval!=0] = fwpuclnt.FwpmEngineOpen0
-
 //sys fwpmEngineClose0(engineHandle windows.Handle) (err error) [failretval!=0] = fwpuclnt.FwpmEngineClose0
 
 //sys fwpmLayerCreateEnumHandle0(engineHandle windows.Handle, enumTemplate *fwpmLayerEnumTemplate0, handle *windows.Handle) (err error) [failretval!=0] = fwpuclnt.FwpmLayerCreateEnumHandle0
-
 //sys fwpmLayerDestroyEnumHandle0(engineHandle windows.Handle, enumHandle windows.Handle) (err error) [failretval!=0] = fwpuclnt.FwpmLayerDestroyEnumHandle0
-
 //sys fwpmLayerEnum0(engineHandle windows.Handle, enumHandle windows.Handle, numEntriesRequested uint32, entries ***fwpmLayer0, numEntriesReturned *uint32) (err error) [failretval!=0] = fwpuclnt.FwpmLayerEnum0
 
 //sys fwpmSubLayerCreateEnumHandle0(engineHandle windows.Handle, enumTemplate *fwpmSublayerEnumTemplate0, handle *windows.Handle) (err error) [failretval!=0] = fwpuclnt.FwpmSubLayerCreateEnumHandle0
-
 //sys fwpmSubLayerDestroyEnumHandle0(engineHandle windows.Handle, enumHandle windows.Handle) (err error) [failretval!=0] = fwpuclnt.FwpmSubLayerDestroyEnumHandle0
-
 //sys fwpmSubLayerEnum0(engineHandle windows.Handle, enumHandle windows.Handle, numEntriesRequested uint32, entries ***fwpmSublayer0, numEntriesReturned *uint32) (err error) [failretval!=0] = fwpuclnt.FwpmSubLayerEnum0
-
-//sys fwpmFreeMemory0(p *struct{}) = fwpuclnt.FwpmFreeMemory0
-
 //sys fwpmSubLayerAdd0(engineHandle windows.Handle, sublayer *fwpmSublayer0, nilForNow *uintptr) (err error) [failretval!=0] = fwpuclnt.FwpmSubLayerAdd0
-
 //sys fwpmSubLayerDeleteByKey0(engineHandle windows.Handle, guid *windows.GUID) (err error) [failretval!=0] = fwpuclnt.FwpmSubLayerDeleteByKey0
 
 //sys fwpmProviderCreateEnumHandle0(engineHandle windows.Handle, enumTemplate *struct{}, handle *windows.Handle) (err error) [failretval!=0] = fwpuclnt.FwpmProviderCreateEnumHandle0
-
 //sys fwpmProviderDestroyEnumHandle0(engineHandle windows.Handle, enumHandle windows.Handle) (err error) [failretval!=0] = fwpuclnt.FwpmProviderDestroyEnumHandle0
-
 //sys fwpmProviderEnum0(engineHandle windows.Handle, enumHandle windows.Handle, numEntriesRequested uint32, entries ***fwpmProvider0, numEntriesReturned *uint32) (err error) [failretval!=0] = fwpuclnt.FwpmProviderEnum0
-
 //sys fwpmProviderAdd0(engineHandle windows.Handle, provider *fwpmProvider0, nilForNow *uintptr) (err error) [failretval!=0] = fwpuclnt.FwpmProviderAdd0
-
 //sys fwpmProviderDeleteByKey0(engineHandle windows.Handle, guid *windows.GUID) (err error) [failretval!=0] = fwpuclnt.FwpmProviderDeleteByKey0
 
 //sys fwpmFilterCreateEnumHandle0(engineHandle windows.Handle, enumTemplate *fwpmFilterEnumTemplate0, handle *windows.Handle) (err error) [failretval!=0] = fwpuclnt.FwpmFilterCreateEnumHandle0
-
 //sys fwpmFilterDestroyEnumHandle0(engineHandle windows.Handle, enumHandle windows.Handle) (err error) [failretval!=0] = fwpuclnt.FwpmFilterDestroyEnumHandle0
-
 //sys fwpmFilterEnum0(engineHandle windows.Handle, enumHandle windows.Handle, numEntriesRequested uint32, entries ***fwpmFilter0, numEntriesReturned *uint32) (err error) [failretval!=0] = fwpuclnt.FwpmFilterEnum0
-
 //sys fwpmFilterAdd0(engineHandle windows.Handle, rule *fwpmFilter0, sd *windows.SECURITY_DESCRIPTOR, id *uint64) (err error) [failretval!=0] = fwpuclnt.FwpmFilterAdd0
-
 //sys fwpmFilterDeleteByKey0(engineHandle windows.Handle, guid *windows.GUID) (err error) [failretval!=0] = fwpuclnt.FwpmFilterDeleteByKey0
 
 //sys fwpmTransactionBegin0(engineHandle windows.Handle, flags uint32) (err error) [failretval!=0] = fwpuclnt.FwpmTransactionBegin0
 //sys fwpmTransactionCommit0(engineHandle windows.Handle) (err error) [failretval!=0] = fwpuclnt.FwpmTransactionCommit0
 //sys fwpmTransactionAbort0(engineHandle windows.Handle) (err error) [failretval!=0] = fwpuclnt.FwpmTransactionAbort0
+
+//sys fwpmFreeMemory0(p *struct{}) = fwpuclnt.FwpmFreeMemory0
